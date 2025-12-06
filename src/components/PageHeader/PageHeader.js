76598d296c474766ef318cc250f62f6177ed7165
Tathagata Chakraborti <tchakra2@outlook.com>
2025-12-05 22:45:07 -0500

    cleanup old projects
    Signed-off-by: Tathagata Chakraborti <tchakra2@outlook.com>

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -10,8 +10,6 @@ import {
   Header,
   HeaderMenuButton,
   HeaderName,
-  // HeaderNavigation,
-  // HeaderMenuItem,
   SkipToContent,
   SideNav,
   SideNavItems,
@@ -68,14 +66,6 @@ class PageHeader extends React.Component {
                 prefix="Tathagata's">
                 Home
               </HeaderName>
-              {/* <HeaderNavigation aria-label="">
-                <HeaderMenuItem
-                  target="_blank"
-                  href="https://www.research.ibm.com/artificial-intelligence"
-                  rel="noopener noreferrer">
-                  IBM Research AI
-                </HeaderMenuItem>
-              </HeaderNavigation> */}
               <SideNav
                 aria-label="Navigation Options"
                 expanded={isSideNavExpanded}>
--- src/content/HomePage/HomePage.js
+++ src/content/HomePage/HomePage.js
@@ -1,77 +1,20 @@
 import React from 'react';
-import {
-  Theme,
-  Grid,
-  Column,
-  // Link,
-  // ProgressStep,
-  // ProgressIndicator,
-} from '@carbon/react';
-
+import { Theme } from '@carbon/react';
 import PageHeader from '../../components/PageHeader';
-// import DATA from './Data.js';
 
-// const ProgressStepNew = props => {
-//   return (
-//     <ProgressStep
-//       style={{ marginBottom: '20px', minHeight: '0', width: '85%' }}
-//       complete={props.props.complete}
-//       current={props.props.current}
-//       label={
-//         <div style={{ marginBottom: '5px' }}>
-//           {!props.props.link && (
-//             <span style={{ color: 'white' }}>{props.props.title}</span>
-//           )}
-//           {props.props.link && (
-//             <Link
-//               style={{ color: 'white' }}
-//               href={props.props.link}
-//               target="_blank">
-//               {props.props.title}
-//             </Link>
-//           )}
-//         </div>
-//       }
-//       secondaryLabel={
-//         <span style={{ color: 'silver' }}>{props.props.description}</span>
-//       }
-//     />
-//   );
-// };
-
-class LandingPage extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-      folded: false,
-    };
-  }
-
-  render() {
-    return (
-      <Theme
-        className="offset"
-        style={{
-          backgroundImage: `url(${process.env.PUBLIC_URL}/images/cover.png)`,
-          backgroundPosition: 'right',
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
-        }}>
-        <PageHeader />
-        <Grid>
-          <Column lg={{ start: 4, end: 8 }} md={{ start: 2, end: 9 }} sm={4}>
-            {/* <ProgressIndicator vertical currentIndex={1} className="news-area">
-              {DATA.map((item, key) => (
-                <React.Fragment key={key}>
-                  <ProgressStepNew props={item} />
-                </React.Fragment>
-              ))}
-            </ProgressIndicator> */}
-          </Column>
-        </Grid>
-      </Theme>
-    );
-  }
-}
+const LandingPage = _ => {
+  return (
+    <Theme
+      className="offset"
+      style={{
+        backgroundImage: `url(${process.env.PUBLIC_URL}/images/cover.png)`,
+        backgroundPosition: 'right',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+      }}>
+      <PageHeader />
+    </Theme>
+  );
+};
 
 export default LandingPage;
